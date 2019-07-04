c5e2418c351a64b21e3ce1491923daae5218d226
Dark_Dmake <dark.dmake@gmail.com>
2019-07-04 16:11:00 +0300

    Добавил логирование

--- dialogflow_api.py
+++ dialogflow_api.py
@@ -2,25 +2,29 @@ import dialogflow
 from google.api_core.exceptions import InvalidArgument
 from google.oauth2.service_account import Credentials
 import os
+import logging
+from log_to_tgm import TelegramBotLogsHandler
 
-def dialog(text,session_id):
+def get_dialog(text,session_id):
     project_id = os.environ['PROJECT_ID']
     lang = 'ru-RU'
-   
-    credentials = Credentials.from_service_account_file("./Dkl-Bot-a247bee41ada.json")
-    
-    session_client=dialogflow.SessionsClient(credentials=credentials)
-    
-    session = session_client.session_path(project_id, session_id)
+    logger = logging.getLogger("Logs To Telegram")
+    logger.setLevel(logging.INFO)
+    logger.addHandler(TelegramBotLogsHandler())
 
-    text_input = dialogflow.types.TextInput(text=text, language_code=lang)
-    query_input = dialogflow.types.QueryInput(text=text_input)
     try:
+        credentials = Credentials.from_service_account_file("./Dkl-Bot-a247bee41ada.json")
+        session_client=dialogflow.SessionsClient(credentials=credentials)
+        
+        session = session_client.session_path(project_id, session_id)
+
+        text_input = dialogflow.types.TextInput(text=text, language_code=lang)
+        query_input = dialogflow.types.QueryInput(text=text_input)
         response = session_client.detect_intent(session=session, query_input=query_input)
-    except InvalidArgument:
-        raise
+    except:
+        logger.critical('Проблема с dialogflow', exc_info=1)
 
     return response.query_result.fulfillment_text
 
 if __name__ == "__main__":
-    dialog('Привет,Проверка',None)
+    get_dialog('Привет,Проверка',None)
