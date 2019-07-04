4197a74f6e407d8426baa42e9ee4ba338bebb556
Dark_Dmake <dark.dmake@gmail.com>
2019-07-04 16:10:41 +0300

    логирование

---
+++ log_to_tgm.py
@@ -0,0 +1,12 @@
+import os
+import telegram
+import logging
+
+class TelegramBotLogsHandler(logging.Handler):
+
+    def emit(self, record):
+        
+        bot = telegram.Bot(token=os.environ['TELEGRAM_LOGBOT_TOKEN'] )
+        bot.send_message(chat_id=os.environ['CHAT_ID_FOR_LOGBOT'], text=self.format(record))
+
+
