e604fdcb75af678c0b9a5c96baf2882a8887d116
Dark_Dmake <dark.dmake@gmail.com>
2019-07-06 21:27:04 +0300

    мелкие правки и форматирование

--- add_intense.py
+++ add_intense.py
@@ -4,19 +4,20 @@ from google.oauth2.service_account import Credentials
 import json
 import os
 
-def create_intent(project_id,display_name,answers,questions):
-    
 
-    credentials = Credentials.from_service_account_file("google-credentials.json")
-    
-    intents_client=dialogflow.IntentsClient(credentials=credentials)
-    
+def create_intent(project_id, display_name, answers, questions):
+
+    credentials = Credentials.from_service_account_file(
+        "google-credentials.json")
+
+    intents_client = dialogflow.IntentsClient(credentials=credentials)
+
     parent = intents_client.project_agent_path(project_id)
     training_phrases = []
 
     for question in questions:
         part = dialogflow.types.Intent.TrainingPhrase.Part(text=question)
-        
+
         training_phrase = dialogflow.types.Intent.TrainingPhrase(parts=[part])
         training_phrases.append(training_phrase)
 
@@ -32,18 +33,18 @@ def create_intent(project_id,display_name,answers,questions):
     except InvalidArgument:
         raise
 
-    
 
 def main():
     project_id = os.environ['PROJECT_ID']
-   
-    with open('./questions.json','r') as file:
-        phrase_file=json.load(file)
 
-    for phrase_list in phrase_file:
-        answers=[phrase_file[phrase_list]['answer'] ]
-        questions=phrase_file[phrase_list]['questions']
-        create_intent(project_id,phrase_list,answers,questions )
+    with open('./questions.json', 'r') as file:
+        phrases = json.load(file)
+
+   
+    for intent in phrases.items():
+        questions=intent[1]['questions']
+        answer=intent[1]['answer']
+        create_intent(project_id, intent[0], [answer], questions)
 
 
 if __name__ == "__main__":
