3329fb3e75a7bcd927b46be2ab53e3b782d0573a
Dark_Dmake <dark.dmake@gmail.com>
2019-07-06 21:27:34 +0300

    форматирование

--- log_to_tgm.py
+++ log_to_tgm.py
@@ -2,12 +2,14 @@ import os
 import telegram
 import logging
 
+
 class TelegramBotLogsHandler(logging.Handler):
 
     def emit(self, record):
-        
-        bot = telegram.Bot(token=os.environ['TELEGRAM_LOGBOT_TOKEN'] )
-        bot.send_message(chat_id=os.environ['CHAT_ID_FOR_LOGBOT'], text=self.format(record))
+
+        bot = telegram.Bot(token=os.environ['TELEGRAM_LOGBOT_TOKEN'])
+        bot.send_message(
+            chat_id=os.environ['CHAT_ID_FOR_LOGBOT'], text=self.format(record))
 
 
 logger = logging.getLogger("Logs To Telegram")
